239918031bd27a0c9ae2aeda217753633b26fde2
mhmohamad80 <mohamadhosseiny63@gmail.com>
2022-08-16 20:57:00 +0430

    changing email and password position from settings.py to environment

--- app/task_drf/settings.py
+++ app/task_drf/settings.py
@@ -168,7 +168,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testingappdjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'cbhivanevmjrhlbm'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 AUTH_USER_MODEL = 'user.User'
